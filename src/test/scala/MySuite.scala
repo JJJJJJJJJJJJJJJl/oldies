259edd688f1e42440d861d4e22798f7923780d8d
JJJJJJJJJJJJJJJl <costa.joaom@hotmail.com>
2023-06-21 17:49:12 +0100

    grand total calc test

--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -1,9 +1,16 @@
-// For more information on writing tests, see
-// https://scalameta.org/munit/docs/getting-started.html
+import domain.*
+
+import java.time.LocalDate
+
 class MySuite extends munit.FunSuite {
-  test("example test that succeeds") {
-    val obtained = 42
-    val expected = 42
-    assertEquals(obtained, expected)
+  test("Test order grand total calculation") {
+    val product: Product = new Product("Chrome Hearts Tumbler", ProductCategory.Tops, 0.5, 1675, LocalDate.now())
+    val item1: Item = new Item(product, 10, 23)
+    val item2: Item = new Item(product, 10, 23)
+    val item3: Item = new Item(product, 10, 23)
+    val item4: Item = new Item(product, 10, 23)
+    val items: List[Item] = List(item1, item2, item3, item4)
+    val order: Order = new Order("JJJJJJ", "999666777", "(666, Panopticon Street, Hell)", items, LocalDate.now())
+    assert(order.grandTotal.equals(8281.0))
   }
 }
