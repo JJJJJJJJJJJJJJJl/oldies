c0b55918a8cb08c4a0bcd706b9362b61019b5fbb
JJJJJJJJJJJJJJJl <costa.joaom@hotmail.com>
2023-06-22 22:05:15 +0200

    small refactor to data utils tests

--- src/test/scala/DateUtilsSuite.scala
+++ src/test/scala/DateUtilsSuite.scala
@@ -5,47 +5,44 @@ import java.time.LocalDate
 
 class DateUtilsSuite extends munit.FunSuite{
   test("Order placement date lies inside the given interval") {
-    // Simulating comand line arguments
-    val minBound: String = "2018-01-01 00:00:00"
-    val maxBound: String = "2019-01-01 00:00:00"
-    val minBoundDate: LocalDate = LocalDate.parse(minBound, DateUtils.dateTimeFormatter)
-    val maxBoundDate: LocalDate = LocalDate.parse(maxBound, DateUtils.dateTimeFormatter)
+    val minOrderBound: String = "2018-01-01 00:00:00"
+    val maxOrderBound: String = "2019-01-01 00:00:00"
+    val minOrderBoundDate: LocalDate = LocalDate.parse(minOrderBound, DateUtils.dateTimeFormatter)
+    val maxOrderBoundDate: LocalDate = LocalDate.parse(maxOrderBound, DateUtils.dateTimeFormatter)
 
     val product: Product = new Product("Chrome Hearts Tumbler", ProductCategory.Tops, 0.5, 1675, LocalDate.parse("2018-01-10 00:00:00", DateUtils.dateTimeFormatter))
     val item: Item = new Item(product, 10, 23)
     val order: Order = new Order("JJJJJJ", "999666777", "(666, Panopticon Street, Hell)", List(item), LocalDate.parse("2018-01-10 00:00:00", DateUtils.dateTimeFormatter))
 
-    assert(order.placementDate.isAfter(minBoundDate))
-    assert(order.placementDate.isBefore(maxBoundDate))
+    assert(order.placementDate.isAfter(minOrderBoundDate))
+    assert(order.placementDate.isBefore(maxOrderBoundDate))
   }
 
   test("Order placement date > interval") {
-    // Simulating comand line arguments
-    val minBound: String = "2018-01-01 00:00:00"
-    val maxBound: String = "2019-01-01 00:00:00"
-    val minBoundDate: LocalDate = LocalDate.parse(minBound, DateUtils.dateTimeFormatter)
-    val maxBoundDate: LocalDate = LocalDate.parse(maxBound, DateUtils.dateTimeFormatter)
+    val minOrderBound: String = "2018-01-01 00:00:00"
+    val maxOrderBound: String = "2019-01-01 00:00:00"
+    val minOrderBoundDate: LocalDate = LocalDate.parse(minOrderBound, DateUtils.dateTimeFormatter)
+    val maxOrderBoundDate: LocalDate = LocalDate.parse(maxOrderBound, DateUtils.dateTimeFormatter)
 
     val product: Product = new Product("Chrome Hearts Tumbler", ProductCategory.Tops, 0.5, 1675, LocalDate.parse("2018-01-10 00:00:00", DateUtils.dateTimeFormatter))
     val item: Item = new Item(product, 10, 23)
     val order: Order = new Order("JJJJJJ", "999666777", "(666, Panopticon Street, Hell)", List(item), LocalDate.parse("2023-01-10 00:00:00", DateUtils.dateTimeFormatter))
 
-    assert(order.placementDate.isAfter(minBoundDate))
-    assert(order.placementDate.isAfter(maxBoundDate))
+    assert(order.placementDate.isAfter(minOrderBoundDate))
+    assert(order.placementDate.isAfter(maxOrderBoundDate))
   }
 
   test("Order placement date < interval") {
-    // Simulating comand line arguments
-    val minBound: String = "2018-01-01 00:00:00"
-    val maxBound: String = "2019-01-01 00:00:00"
-    val minBoundDate: LocalDate = LocalDate.parse(minBound, DateUtils.dateTimeFormatter)
-    val maxBoundDate: LocalDate = LocalDate.parse(maxBound, DateUtils.dateTimeFormatter)
+    val minOrderBound: String = "2018-01-01 00:00:00"
+    val maxOrderBound: String = "2019-01-01 00:00:00"
+    val minOrderBoundDate: LocalDate = LocalDate.parse(minOrderBound, DateUtils.dateTimeFormatter)
+    val maxOrderBoundDate: LocalDate = LocalDate.parse(maxOrderBound, DateUtils.dateTimeFormatter)
 
     val product: Product = new Product("Chrome Hearts Tumbler", ProductCategory.Tops, 0.5, 1675, LocalDate.parse("2018-01-10 00:00:00", DateUtils.dateTimeFormatter))
     val item: Item = new Item(product, 10, 23)
     val order: Order = new Order("JJJJJJ", "999666777", "(666, Panopticon Street, Hell)", List(item), LocalDate.parse("2012-01-10 00:00:00", DateUtils.dateTimeFormatter))
 
-    assert(order.placementDate.isBefore(minBoundDate))
-    assert(order.placementDate.isBefore(maxBoundDate))
+    assert(order.placementDate.isBefore(minOrderBoundDate))
+    assert(order.placementDate.isBefore(maxOrderBoundDate))
   }
 }
