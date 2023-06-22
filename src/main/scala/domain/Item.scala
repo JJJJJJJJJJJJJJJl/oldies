420d6c86698cf5e0f7ecfcc4fa339b60526e7c08
JJJJJJJJJJJJJJJl <costa.joaom@hotmail.com>
2023-06-22 16:45:47 +0200

    item cost turned lazy

--- src/main/scala/domain/Item.scala
+++ src/main/scala/domain/Item.scala
@@ -6,7 +6,7 @@ class Item(
   val taxAmount: Double
   ) {
 
-  val cost: Double = product.price + product.price * taxAmount/100 + shippingFee
+  lazy val cost: Double = product.price + product.price * taxAmount/100 + shippingFee
 
   override def toString(): String = {
     return "Item:[" + cost + ", " + shippingFee + ", " + taxAmount + "]"
