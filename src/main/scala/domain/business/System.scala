f7d8492e8c3008cda7b2fbc902cfae31b6596104
JJJJJJJJJJJJJJJl <costa.joaom@hotmail.com>
2023-06-23 17:19:17 +0200

    System logic bug fixes + System test

--- src/main/scala/domain/business/System.scala
+++ src/main/scala/domain/business/System.scala
@@ -2,7 +2,8 @@ package domain.business
 
 import domain.entities.{Item, Order}
 
-import java.time.{LocalDate, Period}
+import java.time.LocalDate
+import java.time.temporal.ChronoUnit
 
 /* Receives both the interval and data. Returns orders grouped by product age
 **/
@@ -10,17 +11,20 @@ import java.time.{LocalDate, Period}
 class System(
   val orderInterval: List[LocalDate],
   val orders: List[Order],
-  val productAgeSets: List[String] = List("1-3", "4-6", "7-12", ">12")
+  val productAgeSets: List[String]
   ) {
 
   def setContainsInt(set: String, productMonthsOld: Int): Boolean = {
     val splitted: Array[String] = if (set contains "-") set.split("-") else Array(set.stripPrefix(">"))
-    splitted.exists(s => (s.toInt.equals(productMonthsOld)))
+    if (splitted.length.eq(2))
+      splitted.head.toInt.eq(productMonthsOld) || splitted.tail.head.toInt.eq(productMonthsOld)
+      || (productMonthsOld > splitted.head.toInt  && productMonthsOld < splitted.tail.head.toInt)
+    else splitted.head.toInt.eq(productMonthsOld) || splitted.head.toInt < productMonthsOld
   }
 
   def mapProductAgeToAgeSet(presentDate: LocalDate, productCreationDate: LocalDate): String = {
     require(productCreationDate.isBefore(presentDate))
-    val productMonthsOld: Int = Period.between(productCreationDate, presentDate).getMonths
+    val productMonthsOld: Int = ChronoUnit.MONTHS.between(productCreationDate, presentDate).toInt
     val filteredAgeSets: List[String] = productAgeSets.filter(set => setContainsInt(set, productMonthsOld))
     filteredAgeSets.head
   }
@@ -28,12 +32,15 @@ class System(
   def calculcateResults(): Map[String, Int] = {
     val presentDate: LocalDate = LocalDate.now()
     var results: Map[String, Int] = Map[String, Int]()
-    val filteredOrders: List[Order] = orders.filter(order => order.placementDate.isAfter(orderInterval.head) && order.placementDate.isBefore(orderInterval.tail.head))
-    filteredOrders.foreach(order => order.items.foreach(item => {
-      val currentKey:String = mapProductAgeToAgeSet(presentDate, item.product.creationDate)
-      val currentValue = results(currentKey)
-      results = results.updated(currentKey, currentValue + 1)
-    }))
+    val filteredOrders: List[Order] = orders.filter(order => {
+      order.placementDate.isAfter(orderInterval.head) && order.placementDate.isBefore(orderInterval.tail.head)
+    })
+    filteredOrders.foreach(order => {
+      order.items.foreach(item => {
+        val currentKey: String = mapProductAgeToAgeSet(presentDate, item.product.creationDate)
+        results = if (results.contains(currentKey)) results.updated(currentKey, results(currentKey) + 1) else results + (currentKey -> 1)
+      })
+    })
     results
   }
 
---
+++ src/test/scala/SystemSuite.scala
@@ -0,0 +1,35 @@
+import domain.entities.*
+import domain.business.System
+import utils.DateUtils
+
+import java.time.LocalDate
+
+class SystemSuite extends munit.FunSuite {
+  test("System") {
+    val minOrderBound: LocalDate = LocalDate.parse("2018-01-01 00:00:00", DateUtils.dateTimeFormatter)
+    val maxOrderBound: LocalDate = LocalDate.now()
+
+    val productAgeSets: List[String] = List("1-3", "4-6", "7-12", ">12")
+
+    val product1: Product = new Product("Chrome Hearts Tumbler", ProductCategory.Tops, 0.5, 1675, LocalDate.parse("2023-05-01 00:00:00", DateUtils.dateTimeFormatter))
+    val product2: Product = new Product("Chrome Hearts Tumbler", ProductCategory.Tops, 0.5, 1675, LocalDate.parse("2023-02-01 00:00:00", DateUtils.dateTimeFormatter))
+    val product3: Product = new Product("Chrome Hearts Tumbler", ProductCategory.Tops, 0.5, 1675, LocalDate.parse("2022-09-01 00:00:00", DateUtils.dateTimeFormatter))
+    val product4: Product = new Product("Chrome Hearts Tumbler", ProductCategory.Tops, 0.5, 1675, LocalDate.parse("2021-01-01 00:00:00", DateUtils.dateTimeFormatter))
+
+    val item1: Item = new Item(product1, 10, 23)
+    val item2: Item = new Item(product2, 10, 23)
+    val item3: Item = new Item(product3, 10, 23)
+    val item4: Item = new Item(product4, 10, 23)
+
+    val order1: Order = new Order("JJJJJJ", "999666777", "(666, Panopticon Street, Hell)", List(item1, item2, item3, item4), LocalDate.now())
+    val order2: Order = new Order("JJJJJJ", "999666777", "(666, Panopticon Street, Hell)", List(item1, item2, item3, item4), LocalDate.parse("2019-01-01 00:00:00", DateUtils.dateTimeFormatter))
+    val order3: Order = new Order("JJJJJJ", "999666777", "(666, Panopticon Street, Hell)", List(item1, item2, item3, item4), LocalDate.parse("2019-05-01 00:00:00", DateUtils.dateTimeFormatter))
+    val order4: Order = new Order("JJJJJJ", "999666777", "(666, Panopticon Street, Hell)", List(item1, item2, item3, item4), LocalDate.parse("2017-10-13 00:00:00", DateUtils.dateTimeFormatter))
+
+    val system: System = new System(List(minOrderBound, maxOrderBound), List(order1, order2, order3, order4), productAgeSets)
+
+    val results: Map[String, Int] = system.calculcateResults()
+    assert(results.size.eq(4))
+    for ((k,v) <- results) assert(v.eq(2))
+  }
+}
